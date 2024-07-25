8d9c17d28198ca0517e5545b3962766c4248fcc6
JA Tech <87748581+jahin-ahnaf@users.noreply.github.com>
2024-07-25 18:51:23 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import SideBar from "./SideBar"
+import AccountStatus from "./account"
+import Channels from "./Channel"
+import MessageArea from "./MessageArea"
+
+function App() {
+
+  return (
+    <>
+      <SideBar/>
+      <Channels/>
+      <AccountStatus/>
+      <MessageArea/>
+    </>
+  )
+}
+
+export default App
